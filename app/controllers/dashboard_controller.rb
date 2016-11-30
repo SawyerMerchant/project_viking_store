3571b1a6c9ab155cf824fb841da3d92aab08b63d
Alexa <alexa.anderson@outlook.com>
2016-11-30 12:44:10 -0600

    Add gems

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def show
+    @all_users = User.all
+    @all_orders = Order.all
+    @all_products = Product.all
+    @total_revenue = # qty *
   end
 end
