0ee8017136ff921fa02819f61a15cdd263a2e949
John Sawyer <sawyer@sawyermerchant.com>
2016-11-30 15:03:11 -0500

    Total revenue function

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,10 @@
 class DashboardController < ApplicationController
   def show
-    @all_users = User.all
-    @all_orders = Order.all
-    @all_products = Product.all
+    @all_users_count = User.all.count
+    @all_orders_count = Order.all.count
+    @all_products_count = Product.all.count
+    d = Dashboard.new
+    @total_revenue = d.revenue_between(Time.new('1999'), Time.now)
     # @total_revenue = # qty *
   end
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,8 +1,13 @@
 class Dashboard
 
-  def revenue_between(start_date, end_date)
-    this = Product.select('quantity', 'name', 'price').joins("JOIN order_contents ON product_id=products.id").where('order_contents.updated_at >= ? AND order_contents.updated_at <= ?', start_date, end_date)
-    p this
+  def products_sold_between(start_date, end_date)
+    Product.select('quantity', 'name', 'price').joins("JOIN order_contents ON product_id=products.id").joins("JOIN orders ON order_id=orders.id").where('orders.checkout_date >= ? AND orders.checkout_date <= ?', start_date, end_date)
   end
+
+  def revenue_between(start_date, end_date)
+    orders = products_sold_between(start_date, end_date)
+    orders.inject(0) {|revenue, o| revenue + (o.price * o.quantity) }.to_i
+  end
+
 
 end
