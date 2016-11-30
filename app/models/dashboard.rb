028692a817b52594aa0c31d874604418b0302c53
Alexa <alexa.anderson@outlook.com>
2016-11-30 13:35:30 -0600

    begin queries

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,5 +1,8 @@
-class Dashboard < ApplicationRecord
+class Dashboard
 
-  Product.select('*').joins("JOIN order_contents ON product_id=products.id")
+  def revenue_between(start_date, end_date)
+    this = Product.select('quantity', 'name', 'price').joins("JOIN order_contents ON product_id=products.id").where('order_contents.updated_at >= ? AND order_contents.updated_at <= ?', start_date, end_date)
+    p this
+  end
 
 end
